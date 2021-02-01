605746bc76d7249033d11f1662298127cf51ebab
TLIQ <12932894+TLIQ@users.noreply.github.com>
2021-02-01 16:38:47 +0300

    Урок 3

---
+++ app.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+var minimist = require('minimist');
+var _ = require('lodash');
+var readline = require('readline');
+let argv = require('minimist')(process.argv.slice(2));
+const util = require('util');
+var moment = require('moment');
